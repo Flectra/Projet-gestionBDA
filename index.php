fa06c78f3b772cc9b3ea61febc2a8c94fa7fe53c
Unknown <jhennebert@hotmail.fr>
2017-05-19 00:51:03 +0200

    nouveau index

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
-<?php
-	if(!isset($_GET['section']) || $_GET['section']=='index'){
-		include_once('controleur/c_connection.php');
-	else{
-		include_once('controleur/c_accueil.php');
-	}
-	}
-	
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Page d'accueil BDA </title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div class = titreBienvenue>
+		<h2>Bienvenue !</h2> 
+		</div>
+		<div class = "barreNavig">	
+			<ul id="nav">
+		  		<li id="nav-accueil">Accueil</li>
+		  		<li id="nav-clubs">Clubs BDA</li>
+		  		<li id="nav-soon">A venir</li>
+			</ul> 
+		</div>
+	</body>
